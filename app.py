2ab9cd1b2cd7b1d64ab528d970823ab87cc9707d
ilze weppenaar <aweppens@gmail.com>
2020-04-07 08:53:39 -0400

    app.py commit

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+#Testing an update
+
+from flask import Flask, Blueprint
 from views.items import item_blueprint
 
 app = Flask(__name__)
@@ -7,3 +9,4 @@ app.register_blueprint(item_blueprint, url_prefix="/items")
 
 if __name__ == '__main__':
     app.run(debug=True)
+
